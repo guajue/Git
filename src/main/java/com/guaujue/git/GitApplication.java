7af908820980c5b779c28aeaa7723f214a7660a2
guajue <279702656@qq.com>
2019-06-12 00:59:07 +0800

    哈哈哈哈

--- src/main/java/com/guaujue/git/GitApplication.java
+++ src/main/java/com/guaujue/git/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
 		System.out.println(1111111111);
+		System.out.print("222222222222");
 	}
 
 }
